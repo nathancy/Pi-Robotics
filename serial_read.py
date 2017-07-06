77246946f67cfa38808f5327ef2f5fe972221701
Nathan Lam <Lam_Nathan@bah.com>
2017-07-06 02:52:16 +0000

    Clean raw LIDAR reading to cm

--- serial_read.py
+++ serial_read.py
@@ -1,11 +1,13 @@
 import serial
 from pprint import pprint
+import re
+
 ser = serial.Serial('/dev/ttyACM0', 115200)
 
 while True:
-    var = ser.readline()
-    #print(type(var))
-    print(var) 
+    sensor_reading = str(ser.readline())
+    clean_sensor_reading = re.findall(r'\d+', sensor_reading)[0]
+    print(clean_sensor_reading)
     #print("baudrate was", ser.baudrate)
 
 
