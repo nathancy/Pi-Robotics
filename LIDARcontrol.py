82c81e1f4fd41d15ed934cd11f54be070655e92c
Lam-Nathan <Lam-Nathan@bah.com>
2017-07-14 22:22:50 -0400

    Averaged LIDAR readings for better accuracy

--- LIDARcontrol.py
+++ LIDARcontrol.py
@@ -14,6 +14,9 @@ import re
 # Open serial port
 ser = serial.Serial('/dev/ttyACM0', 115200)
 
+# Set number of readings to take, a higher value provides more precise readings
+ACCURACY = 10
+
 class LIDARControl(object):
 
     # Make sure there is only one instance of LIDARControl
@@ -25,15 +28,24 @@ class LIDARControl(object):
             print("ERROR: One instance of LIDARControl is running already.")
             exit(1)
         self._instances.append(self)							 
+
     # Reads the serial port for LIDAR sensor reading from Arduino
     def distance(self):
+       
+        total = 0
 
-        # Read serial port and extract only digits
-        sensor_reading = str(ser.readline())
-        clean_sensor_reading = re.findall(r'\d+', sensor_reading)
-          
-        # Ensure reading is valid 
-        if len(clean_sensor_reading) is 1:
-            if str(clean_sensor_reading[0]).isdigit():
-                return int(clean_sensor_reading[0])
+        # Poll the sensor and average out the readings according to the desired precision
+        for num in range(ACCURACY):
+            # Read serial port and extract only digits
+            sensor_reading = str(ser.readline())
+            clean_sensor_reading = re.findall(r'\d+', sensor_reading)
+              
+            # Ensure reading is valid 
+            if len(clean_sensor_reading) is 1:
+                if str(clean_sensor_reading[0]).isdigit():
+                    total += int(clean_sensor_reading[0])
+
+        # Average the readings
+        total = total/ACCURACY
+        return total
 
