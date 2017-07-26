4969f7ca97339066d0f0cc52266b206d66b960ee
Lam-Nathan <Lam-Nathan@bah.com>
2017-07-26 17:03:41 -0400

    Additional fixes to Car MVP demo

--- demos/MVP/car1_coord.py
+++ demos/MVP/car1_coord.py
@@ -1,3 +1,20 @@
+# Script for MVP demo (Car #1 with Coordinator XBee)
+'''
+Car #1 <- Car #2
+
+Two cars following each other
+
+Scenario #1: Car #1 is trustworthy
+             Car #1 sends stop signal to car #2
+             Car #2 stops
+             Both cars stop
+
+Scenario #2: Car #1 is not trustworthy
+             Car #1 sends stop signal to car #2
+             Car #2 continues on since car #1 is not trusted
+             Both keep going forward
+'''
+
 import motorControl, servoControl, xbeeControl 
 from time import sleep
 import sys
@@ -23,6 +40,8 @@ try:
         print(sys.argv[1], "Scenario #1")
         motor.forward()
         sleep(3)
+
+        # Send stop signal from Car #1 to Car #2
         xbee.send("1", "C")
         motor.stop()
         motor.cleanup()
@@ -33,6 +52,8 @@ try:
         print(sys.argv[1], "Scenario #2")
         motor.forward()
         sleep(3)
+        
+        # Send stop signal from Car #1 to Car #2
         xbee.send("2", "C")
         motor.forward()
         sleep(3)
@@ -45,5 +66,3 @@ except KeyboardInterrupt:
     motor.cleanup()
     sys.exit(1)
 
-
-    
